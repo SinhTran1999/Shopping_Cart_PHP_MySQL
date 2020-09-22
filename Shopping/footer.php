ad26399c85513b08d0bfa5ecafe2d64d4b44b30e
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-22 11:53:19 +0700

    init project

---
+++ Shopping/footer.php
@@ -0,0 +1,7 @@
+</main>
+    <script type="text/javascript" src="../vendor/js/jquery.min.js"></script>
+	<script type="text/javascript" src="../vendor/js/popper.min.js"></script>
+    <script type="text/javascript" src="../vendor/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ Shopping/index.php
@@ -0,0 +1,10 @@
+<?php 
+    // header.php
+    include("header.php");
+?>
+    
+<?php 
+    // footer
+    include("footer.php");
+?>
+
