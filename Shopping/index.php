bae4174d3dec53d566b9b74019d21ff640c99acd
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-25 23:29:30 +0700

    use functions to data transmission easy

--- Shopping/index.php
+++ Shopping/index.php
@@ -1,45 +1,16 @@
 <?php 
     // header.php
     include("header.php");
+    require_once('./php/component.php');
 ?>
     <div class="container">
         <div class="row text-center py-5">
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-                <form action="index.php" method = "post">
-                    <div class="card shadow">
-                        <div>
-                            <img src="./upload/product1.png" alt="Image 1" class ="img-fluid card-img-top">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Product1</h5>
-                            <h6>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </h6>
-                            <p class="card-text">
-                                Some quick example text to buid on the card.
-                            </p>
-                            <h5>
-                                <small><s class="text-secondary">$600</s></small>
-                                <span class="price">$579</span>
-                            </h5>
-                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-                
-            </div>
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-                
-            </div>
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            
-            </div>
+            <?php 
+                component("Product 1", 500, "./upload/product1.png");
+                component("Product 2", 400, "./upload/product2.png");
+                component("Product 3", 350, "./upload/product3.png");
+                component("Product 4", 200, "./upload/product4.png");
+            ?>
         </div>
     </div>
 <?php 
