26ed6c76136f6ba7d69a789344b43016b7e9aedf
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-26 23:42:59 +0700

    crate file cart.php

---
+++ Shopping/cart.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart</title>
+    <link rel="stylesheet" href="../vendor/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fonts/css/all.min.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body class="bg-light">
+    <?php require_once('php/menu.php') ?>;
+    <script type="text/javascript" src="../vendor/js/jquery.min.js"></script>
+	<script type="text/javascript" src="../vendor/js/popper.min.js"></script>
+    <script type="text/javascript" src="../vendor/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
