09301d9c4583f69a6f75674d5392201ad4ca9e44
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-27 08:20:31 +0700

    add info cart shopping and handle add cart

--- Shopping/cart.php
+++ Shopping/cart.php
@@ -1,3 +1,21 @@
+<?php 
+    session_start();
+    require_once('./php/CreateDatabase.php');
+    require_once('./php/component.php');
+
+    $db = new CreateDB("Productdb", "Producttb");
+    if(isset($_POST['remove'])){
+        if($_GET['action'] == 'remove'){
+            foreach($_SESSION['cart'] as $key => $value){
+                if($value["product_id"] == $_GET["id"]){
+                    unset($_SESSION['cart'][$key]);
+                    echo "<script>alert('Product has been Removed...!')</script>";
+                    echo "<script>window.location= 'cart.php'</script>";
+                }
+            }
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +28,62 @@
 </head>
 <body class="bg-light">
     <?php require_once('php/menu.php') ?>;
+    <div class="container-fluid">
+        <div class="row px-5">
+            <div class="col-md-7">
+                <div class="shopping-cart">
+                    <h6>My Cart</h6>
+                    <hr>
+                    <?php 
+                        $total = 0;
+                        if(isset($_SESSION['cart'])){
+                            $product_id = array_column($_SESSION['cart'], 'product_id');
+
+                            // Lấy dữ liệu từ database trên MySQL từ file CreateDatabase.php
+                            $result = $db->getData();
+                            while($row = mysqli_fetch_assoc($result)){
+                                foreach($product_id as $id){
+                                    if($row['id'] == $id){
+                                        cartElement($row['product_image'], $row['product_name'], $row['product_price'], $row['id']);
+                                        $total = $total + (float)$row['product_price'];
+                                    }
+                                }
+                            }
+                        }else{
+                            echo "<h5>Cart is Empty</h5>";
+                        }
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
+                <div class="pt-4">
+                    <h6>PRICE DETAILS</h6>
+                    <hr>
+                    <div class="row price-details">
+                        <div class="col-md-6">
+                            <?php 
+                                if(isset($_SESSION['cart'])){
+                                    $count = count($_SESSION['cart']);
+                                    echo "<h6>Prices ($count items)</h6>";
+                                }else{
+                                    echo "<h6>Price (0 items)</h6>";
+                                }
+                            ?>
+                            <h6>Delivery Charges</h6>
+                            <hr>
+                            <h6>Amount Payable</h6>
+                        </div>
+                        <div class="col-md-6">
+                            <h6>$<?php echo $total;?></h6>
+                            <h6 class="text-success">FREE</h6>
+                            <hr>
+                            <h6>$<?php echo $total; ?></h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script type="text/javascript" src="../vendor/js/jquery.min.js"></script>
 	<script type="text/javascript" src="../vendor/js/popper.min.js"></script>
     <script type="text/javascript" src="../vendor/js/bootstrap.min.js"></script>
